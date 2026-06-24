b038275e364f23ee0b3f2d3104c2a7cfd59d4e21
Renan Venturini <renanventurinioficial@gmail.com>
2026-06-24 10:05:34 -0300

    projeto inicial

---
+++ emprestimos/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div><h3>Projeto Inicial</h3></div>
+    </>
+  )
+}
+
+export default App
